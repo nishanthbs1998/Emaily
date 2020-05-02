9615f82dd18ae319145ce231cf009e66ebc9b8ee
nishanthbs1998 <35337656+nishanthbs1998@users.noreply.github.com>
2020-05-02 11:38:40 +0530

    Installed passport

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express=require("express");
+const passport=require("passport");
+const googleStrat=require("passport-google-oauth20").Strategy;
 const app=express();
+passport.use(googleStrat());
 let port=process.env.port||3000;
 app.get('/',(req,res)=>{
     res.send("WELCOME!!")
