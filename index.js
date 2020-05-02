af22d43abb3c6f7da1fa668691811d87bb3a0a2f
nishanthbs1998 <35337656+nishanthbs1998@users.noreply.github.com>
2020-05-02 13:36:58 +0530

    Added credential keys and passport authentication URLs

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express=require("express");
 const passport=require("passport");
 const googleStrat=require("passport-google-oauth20").Strategy;
+const keys=require("./config/keys")
 const app=express();
-passport.use(googleStrat());
+passport.use(new googleStrat(
+    {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+},
+(accessToken,refreshToken,profile,email)=>{
+    console.log('accessToken',accessToken);
+    console.log('refreshToken',refreshToken);
+    console.log('profile',profile);
+    console.log('email',email);
+}
+)
+);
 let port=process.env.port||3000;
 app.get('/',(req,res)=>{
     res.send("WELCOME!!")
 })
-app.listen(port);
+
+app.get('/auth/google/callback',passport.authenticate('google',{
+    scope:['profile','email']
+}
+)
+);
+
+//app.get('/auth/google/callback',passport.authenticate('google'));
+app.listen(port,()=>{
+    console.log('Server running on port '+port);
+});
