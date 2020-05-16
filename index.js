2e1d092d1601345b522e4eb1c4f7cdeaaa79e5c7
nishanthbs1998 <35337656+nishanthbs1998@users.noreply.github.com>
2020-05-17 00:00:50 +0530

    Added mongo atlas connection and models directory

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express=require("express");
-
+const keys=require("./config/keys");
+const mongoose=require("mongoose");
 const app=express();
-
+require('./services/passport');
+mongoose.connect(keys.mongoURI);
 require('./routes/authRoute')(app);
+
 let port=process.env.port||3000;
 
 
