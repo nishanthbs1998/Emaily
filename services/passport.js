8d66aafa570d83ce8630017ac98da2b027be319f
nishanthbs1998 <35337656+nishanthbs1998@users.noreply.github.com>
2020-05-17 23:37:52 +0530

    Added cookie function and passport functions

--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,36 @@
 const passport=require("passport");
 const googleStrat=require("passport-google-oauth20").Strategy;
-const keys=require("../config/keys")
+const keys=require("../config/keys");
+const mongoose=require("mongoose");
+const User=mongoose.model('users');
+
+passport.serializeUser((user,done)=>{done(null,user.id)});
+
+passport.deserializeUser(async (id,done)=>{let user=await User.findById(id);
+done(null,user);
+})
+
 passport.use(new googleStrat(
     {
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 },
-(accessToken,refreshToken,profile,email)=>{
-    console.log('accessToken',accessToken);
-    console.log('refreshToken',refreshToken);
-    console.log('profile',profile);
-    console.log('email',email);
+async (accessToken,refreshToken,profile,done)=>{
+    let isExistingUser=await User.findOne({googleId:profile.Id});
+    if(isExistingUser){
+        done(null,isExistingUser);
+
+    }else{
+      let newUser= await new User({googleId:profile.id}).save();
+        done(null,newUser);
+    }
+
+    
+    // console.log('accessToken',accessToken);
+    // console.log('refreshToken',refreshToken);
+    // console.log('profile',profile);
+    // console.log('email',email);
 }
 )
 );
