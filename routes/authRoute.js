4faa0f20569dc878971ead9914ed744438c96d66
nishanthbs1998 <35337656+nishanthbs1998@users.noreply.github.com>
2020-05-18 00:32:38 +0530

    Added logout route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,7 +7,12 @@ app.get('/',(req,res)=>{
 app.get('/auth/google/callback',passport.authenticate('google',{
     scope:['profile','email']
 }
-)
-);
-//app.get('/auth/google/callback',passport.authenticate('google'));
+));
+
+app.get('/api/user',(req,res)=>{res.send(req.user)});
+app.get('/api/logout',(req,res)=>{
+    req.logout();
+    res.send(req.user);
+})
+
 };
