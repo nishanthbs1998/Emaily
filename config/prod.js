19cf4c934ff34107ef96b8dad14cda090483abe9
nishanthbs1998 <35337656+nishanthbs1998@users.noreply.github.com>
2020-05-19 13:59:52 +0530

    Created dev and prod env

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports={
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI:process.env.MONGO_URI,
+    cookieKey:process.env.COOKIE_KEY,
+//    mongoProdURI:'mongodb+srv://emailyProdUser:FaGJV8vzkQXBoDj3@emaily-prod-ezmfg.mongodb.net/test?retryWrites=true&w=majority'
+   
+};
